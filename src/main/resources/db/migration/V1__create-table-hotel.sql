9d3716a7965874a7cf631fe2a0300b9cad3c30b6
DELL-I7-05 <DELL-I7-05@sinapsys.com.br>
2024-06-14 13:26:25 -0300

    Feat: add column image

--- src/main/resources/db/migration/V1__create-table-hotel.sql
+++ src/main/resources/db/migration/V1__create-table-hotel.sql
@@ -6,8 +6,7 @@ CREATE TABLE hotels (
     email VARCHAR(255) NOT NULL,
     state VARCHAR(255) NOT NULL,
     country VARCHAR(255) NOT NULL,
-    image varchar(1000),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    image varchar(1000)
 );
--- src/main/resources/db/migration/V2__create-table-rooms.sql
+++ src/main/resources/db/migration/V2__create-table-rooms.sql
@@ -4,8 +4,8 @@ CREATE TABLE rooms (
     type VARCHAR(255) NOT NULL,
     daily_price DECIMAL(10,2) NOT NULL,
     hotel_id INT,
-    image varchar(1000),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    image varchar(1000),
     FOREIGN KEY (hotel_id) REFERENCES hotels(id)
 );
